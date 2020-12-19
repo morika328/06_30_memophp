a98e9c8b9da520a8fef35be92eccb10b7d6d28dc
morika328 <morika328@gmail.com>
2020-12-19 11:12:04 +0900

    second commit

--- index_read.php
+++ index_read.php
@@ -2,11 +2,14 @@
 
 $str = '';
 $file = fopen('data/memo.csv', 'r');
+
+
+
 flock($file, LOCK_EX);
 if ($file) {
     while ($line = fgets($file)) {
         $str .= "<tr><td> {$line} </td></tr>";
-        // var_dump($line[1]);
+        // var_dump($str);
         // exit();
     }
 }
@@ -20,6 +23,30 @@ if ($file) {
 
 flock($file, LOCK_UN);
 fclose($file);
+?>
+
+<?php
+// $csv_file = file_get_contents('data/memo.csv');
+// $aryHoge = explode("\n", $csv_file);
+
+$filer = fopen('data/memo.csv', 'r');
+
+$newAry = array();
+
+while ($line = fgetcsv($filer)) {
+    $newAry[] = $line;
+}
+
+fclose($filer);
+
+// $arycsv = [];
+
+// var_dump($newAry);
+// exit();
+
+// foreach($)
+
+
 ?>
 
 
@@ -49,12 +76,12 @@ fclose($file);
             <dl class="dl1">
                 <div class="namearea">
                     <dt><label for="name">医院名</label></dt>
-                    <dd><?php echo $line; ?></dd>
-                    <dd><textarea class="namebox" type="text" name="name"></textarea></dd>
+                    <dd></dd>
+                    <dd><textarea class="namebox" type="text" name="name"> <?= $newAry[0][0] ?></textarea></dd>
                 </div>
                 <div class="interestarea">
                     <dt><label for="interest">問合せ内容</label></dt>
-                    <dd><input class="interestbox" type="text" name="interest"></dd>
+                    <dd><textarea class="interestbox" type="text" name="interest"><?= $newAry[0][1] ?></textarea></dd>
                 </div>
             </dl>
 
@@ -66,13 +93,13 @@ fclose($file);
                         <ul>
                             <div class="list">
                                 <li class="Dr"> <label for="Dr">Dr</label>
-                                    <input type="text" name="Dr">
+                                    <textarea type="text" name="Dr"><?= $newAry[0][2] ?></textarea>
                                 </li>
                                 <li class="DH"> <label for="DH">DH</label>
-                                    <input type="text" name="DH">
+                                    <textarea type="text" name="DH"><?= $newAry[0][3] ?></textarea>
                                 </li>
                                 <li class="sonota"> <label for="sonota">その他</label>
-                                    <input type="text" name="sonota">
+                                    <textarea type="text" name="sonota"><?= $newAry[0][4] ?></textarea>
                                 </li>
                             </div>
                         </ul>
@@ -85,10 +112,10 @@ fclose($file);
                 <ul>
                     <div class="list">
                         <li class="maker"> <label for="maker">メーカー</label>
-                            <input type="text" name="maker">
+                            <textarea type="text" name="maker"><?= $newAry[0][5] ?></textarea>
                         </li>
                         <li class="kikan"> <label for="kikan">使用歴</label>
-                            <input type="text" name="kikan">
+                            <textarea type="text" name="kikan"><?= $newAry[0][6] ?></textarea>
                         </li>
                     </div>
                 </ul>
@@ -97,17 +124,20 @@ fclose($file);
             <div class="problem_point">
                 <div class="problembox">
                     <h2>問題点</h2>
-                    <textarea name="" name="problem" cols="50" rows="5"></textarea>
+                    <textarea name="" name="problem" cols="50" rows="5"><?= $newAry[0][7] ?></textarea>
                 </div>
 
                 <div class="pointbox">
                     <h2>ポイント</h2>
-                    <textarea name="" name="point" cols="50" rows="5"></textarea>
+                    <textarea name="" name="point" cols="50" rows="5"><?= $newAry[0][8] ?></textarea>
                 </div>
             </div>
             <a href="index.php">入力画面</a>
 </div>
-<td> <?= $str ?> </td>
+<!-- <td> <?= $str ?> </td> -->
+
+
+
 </body>
 
 </html>
